c9b41e7b5100b8fa7cc23ecf1a497e3bc1075037
TheBenedict69 <sabenedict09@gmail.com>
2024-09-15 02:22:44 +0530

    v0.0.1(Team 2)

--- lib/buzzer.dart
+++ lib/buzzer.dart
@@ -1,81 +1,76 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 
 class Buzzer extends StatefulWidget {
-  const Buzzer({Key? key}) : super(key: key);
+  final bool isBuzzerActive;
+  final int startTime;
+  final String userId;
+  final Future<void> Function(
+    int endTime,
+  ) updateBuzzerStatus;
+
+  const Buzzer({
+    Key? key,
+    required this.isBuzzerActive,
+    required this.startTime,
+    required this.userId,
+    required this.updateBuzzerStatus,
+  }) : super(key: key);
 
   @override
   State<Buzzer> createState() => _BuzzerState();
 }
 
 class _BuzzerState extends State<Buzzer> {
-  final _firestore = FirebaseFirestore.instance;
-  bool _isBuzzerActive = false;
+  final AssetsAudioPlayer _assetsAudioPlayer = AssetsAudioPlayer();
+  int endTime = 0;
+  bool _isLastPressed = false;
 
   @override
-  void initState() {
-    super.initState();
-    _firestore
-        .collection('buzzer_data')
-        .doc('data')
-        .snapshots()
-        .listen((snapshot) {
-      final data = snapshot.data();
-      setState(() {
-        _isBuzzerActive = data?['onPressed'] ?? false;
-      });
-    });
+  void dispose() {
+    _assetsAudioPlayer.dispose();
+    super.dispose();
+  }
+
+  Future<void> _playBuzzerSound() async {
+    try {
+      await _assetsAudioPlayer.open(
+        Audio('assets/mp3/buzzer.mp3'),
+        autoStart: true,
+      );  
+    } catch (e) {
+      print("Error playing buzzer sound: $e");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        SizedBox(
-          width: MediaQuery.sizeOf(context).width * 0.5,
-          height: MediaQuery.sizeOf(context).height * 0.5,
-          child: FloatingActionButton(
-            shape: const CircleBorder(),
-            onPressed: !_isBuzzerActive
-                ? () async {
-                    await _firestore
-                        .collection('buzzer_data')
-                        .doc('data')
-                        .update(
-                      {'onPressed': true},
-                    );
-                  }
-                : null,
-            child: Text(
-              'Buzzer',
-              style:
-                  TextStyle(fontSize: MediaQuery.sizeOf(context).width * 0.1),
-            ),
-          ),
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width * 0.5,
+      height: MediaQuery.sizeOf(context).height * 0.5,
+      child: FloatingActionButton(
+        backgroundColor: !widget.isBuzzerActive && !_isLastPressed
+            ? Color.fromARGB(255, 237, 63, 60)
+            : Colors.grey, // Gray out the button when disabled
+        elevation: 30,
+        splashColor: Colors.red,
+        shape: const CircleBorder(),
+        onPressed: !widget.isBuzzerActive && !_isLastPressed
+            ? () async {
+                endTime = DateTime.now().millisecondsSinceEpoch;
+                await widget.updateBuzzerStatus(
+                  endTime,
+                );
+                await _playBuzzerSound(); // Play the buzzer sound
+              }
+            : null,
+        child: Text(
+          'Buzzer',
+          style: TextStyle(
+              fontSize: MediaQuery.sizeOf(context).width * 0.1,
+              color: Colors.black),
         ),
-        // SizedBox(
-        // height: MediaQuery.sizeOf(context).height * 0.01,
-        // ),
-        SizedBox(
-          width: MediaQuery.sizeOf(context).width * 0.5,
-          height: MediaQuery.sizeOf(context).height * 0.5,
-          child: FloatingActionButton(
-            shape: const CircleBorder(),
-            onPressed: () async {
-              await _firestore.collection('buzzer_data').doc('data').update(
-                {'onPressed': false},
-              );
-            },
-            child: Text(
-              'Reset',
-              style: TextStyle(
-                fontSize: MediaQuery.sizeOf(context).width * 0.1,
-              ),
-            ),
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:buzzer2/buzzer.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
@@ -8,16 +9,101 @@ Future<void> main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  bool _isBuzzerActive = false;
+  int startTime = 0;
+  final String userId = "Team2"; // Using Team1
+  String lastUID =
+      "null"; // Properly naming the variable instead of ColorChanging
+
+  @override
+  void initState() {
+    super.initState();
+    _listenToBuzzerData();
+  }
+
+  Future<void> _listenToBuzzerData() async {
+    _firestore.collection('buzzer_data').doc('data').snapshots().listen(
+      (snapshot) {
+        final data = snapshot.data();
+        if (data != null) {
+          setState(() {
+            _isBuzzerActive = data['onPressed'] ?? false;
+            startTime = data['startTime'] ?? 0;
+            lastUID = data['lastUID'] ?? "null"; // Updating lastUID properly
+          });
+        }
+      },
+    );
+  }
+
+  Future<void> updateBuzzerStatus(int endTime) async {
+    await _firestore.collection('buzzer_data').doc('data').update({
+      'onPressed': true,
+      'lastUID': "Team2", // This will now store 'Team1'
+      'endTeam2': endTime, // Storing endTime for Team1
+    });
+    Future.delayed(Duration(milliseconds: 5));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Buzzer(),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.black,
+                const Color.fromRGBO(31, 31, 31, 1),
+                _isBuzzerActive && lastUID == "Team2"
+                    ? Colors
+                        .red // Set the background to red if this team pressed
+                    : _isBuzzerActive
+                        ? Colors
+                            .grey // Set the background to grey if other team pressed
+                        : Colors.lightGreen, // Set to green initially
+              ],
+            ),
+          ),
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(height: MediaQuery.sizeOf(context).height * 0.1),
+                Image.asset(
+                  'assets/images/mindcraft.png',
+                  width: MediaQuery.sizeOf(context).width * 0.7,
+                ),
+                SizedBox(height: MediaQuery.sizeOf(context).height * 0.06),
+                Text(
+                  "Team 2", // Displaying the team name
+                  style: TextStyle(
+                      color: const Color.fromARGB(255, 137, 134, 134),
+                      fontWeight: FontWeight.w900,
+                      fontSize: MediaQuery.sizeOf(context).width * 0.13),
+                ),
+                SizedBox(height: MediaQuery.sizeOf(context).height * 0.03),
+                // Pass the necessary data to the Buzzer widget
+                Buzzer(
+                  isBuzzerActive: _isBuzzerActive,
+                  startTime: startTime,
+                  userId: userId,
+                  updateBuzzerStatus: updateBuzzerStatus,
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
